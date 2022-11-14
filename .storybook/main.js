81f2663a9d25bca34aa41ae8e7861cfdbbaab3ba
dominicom <dominik.kiepuszewski@gmail.com>
2022-11-14 21:06:20 +0100

    Added sass to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,15 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-sass-postcss"
   ],
   "framework": "@storybook/react"
 }
