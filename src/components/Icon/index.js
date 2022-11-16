c3ee2601d3f5f2e7fc814160b6bc79b4056a2df4
dominicom <dominik.kiepuszewski@gmail.com>
2022-11-16 13:48:28 +0100

    Added components: Button, Icon. Styles: typography and colors. To-do: stories

---
+++ src/components/Icon/index.js
@@ -0,0 +1,16 @@
+import { SWMIcon } from 'react-swm-icon-pack';
+
+export default function Icon({ type, className, size, color, set, stroke }) {
+  const classes = [className && className, "spbx--icon"].join(' ').trim();
+  return (
+    <span className={classes}>
+      <SWMIcon
+        name={type || "Circle"}
+        color={color || "currentColor"}
+        set={set || "oultine"}
+        size={size || 16}
+        strokeWidth={stroke || 2}
+      />
+    </span>
+  );
+}
